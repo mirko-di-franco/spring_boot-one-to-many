90816ce05d958586cdeaf151fb4a9b3e855fb5f6
Mirko Di Franco <120678275+mirko-di-franco@users.noreply.github.com>
2024-05-18 08:34:54 +0200

    first

---
+++ src/main/java/com/generation/relazionionetomany/RelazioniOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.generation.relazionionetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelazioniOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelazioniOneToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/relazionionetomany/RelazioniOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.relazionionetomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelazioniOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
